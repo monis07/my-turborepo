0f4db4c53b891ab9525a6a67b2cec307d0eaf099
monis07 <monisazeem@gmail.com>
2024-09-14 00:31:23 +0530

    spelling correction

--- apps/next-app/deploy.sh
+++ apps/next-app/deploy.sh
@@ -3,7 +3,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.17.0/bin
 # this line gives us access to node, npm in this file. it is helpful if we are using node or npm in this file
 
 cd /home/ubuntu/my-turborepo
-git pull origin master
+git pull origin main
 npm install
 npm run build
 pm2 stop next
